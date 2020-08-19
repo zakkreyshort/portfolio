993ae64f3caa54c27fc6b419c2360648256bde49
Zakkrey Short <zakkreyjshort@gmail.com>
2020-08-18 17:30:06 -0700

    Updates

---
+++ precache-manifest.2147a96d978c2f515303402ed0083dae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a5c77b1601733403b0643d2bc71b06a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "010f7ea7c8b9749f94b4",
+    "url": "/portfolio/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "cb7ba5bdd5792d39f0fa",
+    "url": "/portfolio/static/js/2.1e077a8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.1e077a8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "010f7ea7c8b9749f94b4",
+    "url": "/portfolio/static/js/main.d5ec8ecd.chunk.js"
+  },
+  {
+    "revision": "40c8b1c96ce8d5b881bc",
+    "url": "/portfolio/static/js/runtime-main.0cff674d.js"
+  }
+]);
